ed4c4bac8d2ce0c25e64b33fb1908ffd8cc834e7
Caitlin <mug68tum@uni-duesseldorf.de>
2025-06-13 01:55:31 +0200

    add gui

--- src/main/java/de/justataschenrechner/Controller.java
+++ src/main/java/de/justataschenrechner/Controller.java
@@ -1,10 +1,15 @@
 package de.justataschenrechner;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+import java.util.Stack;
+
 public class Controller {
+    private String inputNumbers = "";
+
     @FXML
     private Label outputField;
 
@@ -70,4 +75,127 @@ public class Controller {
 
     @FXML
     private Label secretHorseLabel;
+
+
+    private void showInputNumbers() {
+        outputField.setText(inputNumbers);
+    }
+
+    @FXML
+    public void initialize() {
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                showInputNumbers();
+            }
+        };
+        timer.start();
+    }
+
+    @FXML
+    private void typeOne() {
+        inputNumbers += "1";
+    }
+
+    @FXML
+    private void typeTwo() {
+        inputNumbers += "2";
+    }
+
+    @FXML
+    private void typeThree() {
+        inputNumbers += "3";
+    }
+
+    @FXML
+    private void typeFour() {
+        inputNumbers += "4";
+    }
+
+    @FXML
+    private void typeFive() {
+        inputNumbers += "5";
+    }
+
+    @FXML
+    private void typeSix() {
+        inputNumbers += "6";
+    }
+
+    @FXML
+    private void typeSeven() {
+        inputNumbers += "7";
+    }
+
+    @FXML
+    private void typeEight() {
+        inputNumbers += "8";
+    }
+
+    @FXML
+    private void typeNine() {
+        inputNumbers += "9";
+    }
+
+    @FXML
+    private void typeZero() {
+        inputNumbers += "0";
+    }
+
+    @FXML
+    private void deleteAll() {
+        inputNumbers = "";
+    }
+
+    @FXML
+    private void delete() {
+        String tempNumbers = "";
+        for(int i = 0; i < inputNumbers.length() - 1; i++) {
+            tempNumbers += inputNumbers.charAt(i);
+        }
+        inputNumbers = tempNumbers;
+    }
+
+    @FXML
+    private void plus() {
+        inputNumbers += "+";
+    }
+
+    @FXML
+    private void minus() {
+        inputNumbers += "-";
+    }
+
+    @FXML
+    private void multi() {
+        inputNumbers += "x";
+    }
+
+    @FXML
+    private void div() {
+        inputNumbers += "/";
+    }
+
+    @FXML
+    private void komma(){
+        inputNumbers += ".";
+    }
+
+    @FXML
+    private void klammerLinks(){
+        inputNumbers = "(";
+    }
+
+    @FXML
+    private void klammerRechts(){
+        inputNumbers += ")";
+    }
+
+    @FXML
+    private void equal(){
+        //TODO
+        String speicher = inputNumbers;
+        inputNumbers = "Hello World";
+    }
 }
